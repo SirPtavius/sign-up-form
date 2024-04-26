df04172eac28052899775e124cb663cf6e9aefe3
Gabriele <124803038+SirPtavius@users.noreply.github.com>
2024-04-26 17:38:22 +0200

    Change font, added credit for image author, added error when passwords missmatch in JS, small improvement to sizes and styles

---
+++ script.js
@@ -0,0 +1,18 @@
+let button = document.getElementById("createAccount");
+
+button.addEventListener("click", (event) => {
+    let password = document.getElementById("pass")
+    let confirmPassword = document.getElementById("confirm_password")
+    let passwordError = document.getElementById("password_error");
+
+
+    if (confirmPassword.value !== password.value) {
+        event.preventDefault(); // Evita il comportamento predefinito del pulsante
+        passwordError.textContent = "Passwords do not match";
+        password.style.borderColor = "red";
+        confirmPassword.style.borderColor = "red";
+    } else {
+        password.textContent = ""; // Rimuove il messaggio di errore
+        confirmPassword.style.borderColor = ""; // Reimposta il colore del bordo
+    }
+});
